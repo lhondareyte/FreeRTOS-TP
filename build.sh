b8f9638fb381314c5b9fda4e45cd401494709f14
Luc Hondareyte <lhondareyte@users.noreply.github.com>
2026-04-19 12:32:35 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 #
-
+if [ ! -f FreeRTOS/CMakeLists.txt ] ; then
+	git submodule init
+	git submodule update --recursive
+fi
 cmake -B build -S . #-DFREERTOS_KERNEL_DIR=./FreeRTOS/kernel
 cmake --build build
